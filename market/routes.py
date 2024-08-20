1fe39e95ff22611179a829842f1521cbc2998804
Keeper <fmartinez@merlindataquality.com>
2024-08-20 20:51:35 -0300

    finish project

--- market/routes.py
+++ market/routes.py
@@ -16,6 +16,7 @@ def home_page():
 @login_required
 def market_page():
     purchase_form = PurchaseItemForm()
+    selling_form = SellItemForm()
     if request.method == "POST":
         purchased_item = request.form.get("purchased_item")
         p_item_object = Item.query.filter_by(name=purchased_item).first()
@@ -31,28 +32,32 @@ def market_page():
                     f"Unfortunately, you don't have enough money to purchase {p_item_object.name}",
                     category="danger",
                 )
-        return redirect(url_for("market_page"))
-
-    sell_form = SellItemForm()
-    if request.method == "POST":
         sold_item = request.form.get("sold_item")
         s_item_object = Item.query.filter_by(name=sold_item).first()
         if s_item_object:
-            if current_user.id == s_item_object.owner:
-                s_item_object.owner = None
-                current_user.budget += s_item_object.price
-                db.session.commit()
+            if current_user.can_sell(s_item_object):
+                s_item_object.sell(current_user)
                 flash(
-                    f"Congratulations! You sold {s_item_object.name} back to market!",
+                    f"Congratulations! You sold {s_item_object.name}",
                     category="success",
                 )
-    # items = Item.query.all()
-    # items = Item.query.filter_by(owner=None)
+            else:
+                flash(
+                    f"Something went wrong with selling {s_item_object.name}",
+                    category="danger",
+                )
+        return redirect(url_for("market_page"))
+
 
     if request.method == "GET":
         items = Item.query.filter_by(owner=None)
+        owned_items = Item.query.filter_by(owner=current_user.id)
         return render_template(
-            "market.html", items=items, purchase_form=purchase_form, sell_form=sell_form
+            "market.html",
+            items=items,
+            purchase_form=purchase_form,
+            owned_items=owned_items,
+            selling_form=selling_form,
         )
 
 
